954d343fb3c0f77ada3de987c64eaa5ade36f8e3
Prajwal-Zoting <prajwalzoting2002@gmail.com>
2023-08-03 19:54:33 +0530

    commit

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // we link the router files to make our route easy 
 app.use(require('./router/auth'));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.get('/signup', (req, res) => {
     res.send(`Hello Registration world from the server`);
